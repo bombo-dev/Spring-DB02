0f29ae2af564d96ab05d13546bf76dcde77248c3
Bombo <bombo96@chungwoon.ac.kr>
2023-03-17 00:07:32 +0900

    [Spring Data JPA 쿼리 오류 수정 Equals -> Equal]

--- src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
+++ src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -13,7 +13,7 @@ public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThanEqual(Integer price);
 
     // 쿼리 메서드 (아래 메서드와 같은 기능 수행)
-    List<Item> findByItemNameLikeAndPriceLessThanEquals(String itemName, Integer price);
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
 
     // 서비스 로직에서는 단일 책임 원칙을 지켜서 함수명을 줄이는 것이 좋지만, 쿼리 같은 경우는 조건자때문에 함수명이 길어지는 문제가 있다.
     // 때문에 길게 늘리지 말고 추상화된 메서드 명으로 주석을 달아놓는 것이 더 좋다.
