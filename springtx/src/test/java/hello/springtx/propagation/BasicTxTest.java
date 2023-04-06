d5ab1ec2de13c07433b45ff12a62237edc55b79b
Bombo <bombo96@chungwoon.ac.kr>
2023-04-06 22:44:47 +0900

    [트랜잭션 전파 시 논리 내부 트랙잭션 커밋의 동작 과정]

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -80,4 +80,24 @@ public class BasicTxTest {
         txManager.rollback(tx2);
         log.info("트랜잭션2 롤백 완료");
     }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        // DefaultTransactionAttribute 가 DefaultTransactionDefault 를 상속받아 더 많은 기능을 가지고 있음.
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        // 트랜잭션 동기화 매니저를 확인하고, 기존에 진행되던 트랜잭션이 존재한다면 기존 트랜잭션에 참여한다.
+        // Participating in existing transaction
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        // 이 과정에서 실제로 커밋이 발생하지 않는다. 실제로 커밋이 되버리면 물리 트랜잭션이 커밋되는거나 다름이 없기 때문.
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
 }
