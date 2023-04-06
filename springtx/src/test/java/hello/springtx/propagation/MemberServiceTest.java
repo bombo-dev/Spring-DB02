e38d92d3405a6d67bb6fa7538f1cacae6e200810
Bombo <bombo96@chungwoon.ac.kr>
2023-04-07 02:20:36 +0900

    [트랜잭션 전파 활용 저장소에만 트랜잭션 적용 상태, 예외 처리 정상 흐름 테스트]

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -37,4 +37,24 @@ class MemberServiceTest {
         Assertions.assertThat(memberRepository.find(username).isPresent()).isTrue();
         Assertions.assertThat(logRepository.find(username).isPresent()).isTrue();
     }
+
+    /**
+     * memberService @Transactional:OFF
+     * memberRepository @Transactional:ON
+     * logRepository @Transactional : ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        Assertions.assertThatThrownBy(() -> memberService.joinV1(username))
+                        .isInstanceOf(RuntimeException.class);
+
+        //then
+        // 예외 발생 시 정상 흐름이 되도록 처리해두었음.
+        Assertions.assertThat(memberRepository.find(username).isPresent()).isTrue();
+        Assertions.assertThat(logRepository.find(username).isPresent()).isFalse();
+    }
 }
