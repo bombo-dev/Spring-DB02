e8a13a060b3ace058c165ec66b0b4429746f140a
Bombo <bombo96@chungwoon.ac.kr>
2023-04-08 00:24:13 +0900

    [@Transactional REQUIRED 트랜잭션 예외 복구 상황, 불가능]

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 @Slf4j
 @SpringBootTest
@@ -94,5 +95,47 @@ class MemberServiceTest {
         //then
         Assertions.assertThat(memberRepository.find(username).isPresent()).isTrue();
         Assertions.assertThat(logRepository.find(username).isPresent()).isTrue();
+    }
+
+    /**
+     * memberService @Transactional : ON
+     * memberRepository @Transactional : ON
+     * logRepository @Transactional : ON Exception
+     */
+    // 각 트랜잭션들이 참여를 하고, 하나의 논리 트랜잭션에서 에외가 발생해 처리를 하지 못하면 rollbackOnly = true 로 변환.
+    @Test
+    void outerTxOn_fail() {
+        // given
+        String username = "로그예외_outerTxOn_fail";
+
+        // when
+        Assertions.assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        // then
+        // 예외 발생 시 물리 트랜잭션은 롤백된다.
+        Assertions.assertThat(memberRepository.find(username).isPresent()).isFalse();
+        Assertions.assertThat(logRepository.find(username).isPresent()).isFalse();
+    }
+
+    /**
+     * memberService @Transactional : ON
+     * memberRepository @Transactional : ON
+     * logRepository @Transactional : ON Exception
+     */
+    // 각 트랜잭션들이 참여를 하고, 하나의 논리 트랜잭션에서 에외가 발생해 처리를 하지 못하면 rollbackOnly = true 로 변환.
+    @Test
+    void recoverException_fail() {
+        // given
+        String username = "로그예외_recoverException_fail";
+
+        // when
+        Assertions.assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
+
+        // then
+        // 예외를 잡아서 회원 저장에 대한 서비스 로직은 True 로 예상하나, rollbackOnly = true 옵션으로 인하여, UnExpectedRollback 예외가 발생한다.
+        Assertions.assertThat(memberRepository.find(username).isPresent()).isFalse();
+        Assertions.assertThat(logRepository.find(username).isPresent()).isFalse();
     }
 }
